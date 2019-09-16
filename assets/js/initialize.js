68705108fb6331ba5f26d9895b8989aa0f410438
thylux <thylux@gmail.com>
2019-09-16 23:04:31 +0100

    Technologies in projects

--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -1,6 +1,4 @@
 function initializeScripts(){
-  // $( document ).tooltip();
-
   $('.materialboxed').materialbox();
   $(".button-collapse").sideNav({
     menuWidth: 200,
@@ -8,25 +6,6 @@ function initializeScripts(){
     closeOnClick: true,
     draggable: true
   });
-  // $('.carousel-arrows').click(function(e){
-  //   var arrow = e.target;
-  //   debugger
-  // })
-
-  $('.email_me').submit(function(e){
-    e.preventDefault();
-    debugger
-    $.ajax({
-        url: $(e.target).attr('action'),
-        method: "POST",
-        data: $('.email_me').serialize(),
-        dataType: "json"
-    }).done(function(res) {
-      $('.email_input').val('');
-      $('.email_input').removeClass('valid');
-      $('#email_modal').modal('open');
-    })
-  })
 
   $('.my-borders').css({
     height: '0px',
@@ -36,45 +15,6 @@ function initializeScripts(){
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
 
   $('#textarea1').trigger('autoresize');
-
-  $('.project_list').on('click', '.next', function(e){
-    e.preventDefault();
-    var thisId = $(this).attr('id');
-    var nextId = (parseInt(thisId) + 1).toString();
-
-    $('#' + thisId).modal('close');
-
-    // need to rework this to use promise rather then setTimeout
-    setTimeout(function(){
-      var nextId = (parseInt(thisId) + 1).toString();
-      $('#' + nextId).modal('open')
-    }, 200)
-  })
-
-  $('#email_modal').modal({
-    opacity: .8, // Opacity of modal background
-    inDuration: 200, // Transition in duration
-    outDuration: 200, // Transition out duration
-    startingTop: '30%', // Starting top style attribute
-    endingTop: '35%',
-  })
-
-  $('.project_list .modal').modal({
-      dismissible: true, // Modal can be dismissed by clicking outside of the modal
-      opacity: .8, // Opacity of modal background
-      inDuration: 200, // Transition in duration
-      outDuration: 200, // Transition out duration
-      startingTop: '0%', // Starting top style attribute
-      endingTop: '0%', // Ending top style attribute
-      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-        // alert("Ready");
-        modal_open = true
-        $('.carousel.carousel-slider').carousel({fullWidth: true});
-      },
-      complete: function(e) {
-        // debugger
-      } // Callback for Modal close
-    });
 
   var urlString = window.location.href;
   
@@ -151,9 +91,9 @@ function initializeScripts(){
     $("html, body").animate({ scrollTop: 0 }, 250);
     return false;
   })
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems, options);
-  });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  M.Tooltip.init(elems, options);
+});
